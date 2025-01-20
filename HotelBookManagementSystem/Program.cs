94ce323652bfd459036d6136bf7087bc0db97321
Digambar Kothawale <digambarkothawale05@gmail.com>
2025-01-20 13:58:16 +0530

    Add project files.

---
+++ HotelBookManagementSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Room
+{
+    public int Roomnumber { get; set; }
+    public string Type { get; set; }
+    public Decimal Price { get; set; }
+    public bool Isavailable { get; set; }
+
+    public void ShowRoomDetails()
+    {
+        Console.WriteLine($"Roomnumber : {Roomnumber} - Type : {Type} -  Price : {Price} - Isavailable : {Isavailable} ");
+    }
+}
